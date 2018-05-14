6cc4ed641b3ba4fba57ac6c69a894754de80e92f
Tiphaine Viard <tiphaine.viard@riken.jp>
2018-05-14 11:50:34 +0900

    Setup files for packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'streamfig',
+  packages = ['streamfig'], # this must be the same as the name above
+  version = '1',
+  description = 'Small library to draw stream graphs into FIG format',
+  author = 'Tiphaine Viard',
+  author_email = 'tiphaine.viard@riken.jp',
+  url = 'https://github.com/TiphaineV/streamfig', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['stream graphs', 'visualization', 'temporal networks', 'xfig'], # arbitrary keywords
+  classifiers = [],
+)
