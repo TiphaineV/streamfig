2a8d49494ca39bc5e40e22b87ff6687bb8a51beb
Tiphaine Viard <tiphaine.viard@telecom-paris.fr>
2021-05-06 11:54:33 +0200

    Setup modified for 1.2

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ import setuptools
 
 setuptools.setup(
   name = 'streamfig',
-  version = '1.1.6',
+  version = '1.2',
   description = 'Draw stream graphs into FIG format',
   author = 'Tiphaine Viard',
   author_email = 'tiphaine.viard@telecom-paris.fr',
   url = 'https://github.com/TiphaineV/streamfig', 
-  download_url = 'https://github.com/TiphaineV/streamfig/archive/1.1.6.tar.gz', 
+  download_url = 'https://github.com/TiphaineV/streamfig/archive/1.2.tar.gz', 
   keywords = ['stream graphs', 'visualization', 'temporal networks', 'xfig'], 
   classifiers = [],
   packages=["streamfig"]
